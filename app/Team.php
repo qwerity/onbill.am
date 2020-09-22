18f5f76891926861b26ff2ede02fca4e51c8b40e
Laravel Daily <info@laraveldaily.com>
2020-09-22 19:31:39 +0000

    QuickAdminPanel automatic commit

--- app/Team.php
+++ app/Team.php
@@ -32,6 +32,21 @@ class Team extends Model
         return $date->format('Y-m-d H:i:s');
     }
 
+    public function teamServices()
+    {
+        return $this->hasMany(Service::class, 'team_id', 'id');
+    }
+
+    public function teamClients()
+    {
+        return $this->hasMany(Client::class, 'team_id', 'id');
+    }
+
+    public function teamPayments()
+    {
+        return $this->hasMany(Payment::class, 'team_id', 'id');
+    }
+
     public function owner()
     {
         return $this->belongsTo(User::class, 'owner_id');
